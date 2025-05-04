339c511ebb94f3a5d52ac8093a0dcb8b4ea7fb5c
jcarin <jeremy.carin@gmail.com>
2025-05-04 15:30:07 -0400

    setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,24 @@
+dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
 # apt
 sudo apt update && sudo apt -y upgrade
-
+sudo apt install -y git curl wget vim htop tree
 
 # vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# install dotfiles
+ln -sf "$dir/gitconfig" ~/.gitconfig
+ln -sf "$dir/gitignore" ~/.gitignore
+ln -sf "$dir/bash_aliases" ~/.bash_aliases
+
+# setup rust
+if ! command -v rustc &> /dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
+
+# install uv
+if ! command -v uv &> /dev/null; then
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+fi
